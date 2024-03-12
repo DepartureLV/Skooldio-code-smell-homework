4f6fe932855d2fe2c640579667273cd7ac3a296b
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-03-13 00:49:00 +0700

    test to check if /total work

--- tests/total.test.js
+++ tests/total.test.js
@@ -1,3 +1,5 @@
+const request = require("supertest");
+const appTotal = require("../app-total");
 const total = require("../total");
 /* test('total of empty string should be 0', () => {
     expect(total('')).toBe(0);
@@ -12,7 +14,23 @@ describe("Total Test", () => {
   it("should return 0 if input is empty string", () => {
     expect(total("")).toBe(0);
   });
-  it('should return 1 if input is more than 1000', () => {
-    expect(total('1,1001')).toBe(1);
-  })
+  it("should return 1 if input is more than 1000", () => {
+    expect(total("1,1001")).toBe(1);
+  });
+
+  it("POST / => Convert", () => {
+    return request(appTotal)
+      .post("/total")
+      .send({
+        values: "1,2",
+      })
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        expect(response.body).toEqual(225);
+      });
+  });
+
+  it("POST /total => item values correct data type check", () => {
+    return request(appTotal).post("/total").send({ values: "1,2" }).expect(200);
+  });
 });

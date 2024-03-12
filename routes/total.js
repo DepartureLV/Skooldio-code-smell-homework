32a963a8f10d58a8d8067fbf9536d5d63b2405d4
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-03-13 00:50:56 +0700

    remove unrelated/ excessive route

--- routes/total.js
+++ routes/total.js
@@ -4,18 +4,6 @@ const createError = require('http-errors');
 const total = require('../total')
 const convert = require('../convert')
 
-// Products Array
-router.post('/', (req, res, next) => {
-    const { body } = req;
-    if (typeof body.name !== 'string') {
-        return next(createError(400, 'Validation Error'));
-    }
-    var newId = products.length + 1;
-    const newProduct = { id: "'" + newId + "'", name: body.name, inStock: false, };
-    products.push(newProduct);
-    res.status(201).json(newProduct);
-});
-
 router.post('/total', (req, res) => {
     const { body } = req;
     var values = body.values;
